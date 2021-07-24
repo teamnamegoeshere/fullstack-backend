23a2c148917f89341dd2f1cfa7118018437049cc
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-07-24 22:22:08 +1000

    add log_in route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,18 @@ class UsersController < ApplicationController
             render json: @user.errors, status: :unprocessable_entity
         end
     end
+
+    def log_in
+        @user = User.find_by_email(params[:email])
+
+        if @user && @user.authenticate(params[:password])
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+            render json: {username: @user.username, jwt: auth_token.token}, status: 200
+        else
+            render json: {error: "incorrect username or password"}, status: 404
+        end
+    end
+    
     private
     def user_params
         params.permit(:username, :email, :password, :password_confirmation, :first_name, :last_name, :date_of_birth)
