5e0cda1c21629aff9351a978cf5ec0b3164101af
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-07-24 22:02:33 +1000

    add jwt to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         if @user.save
-            render json: @user, status: :created
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+            # render json: @user, status: :created
+            render json: {username: @user.username, jwt: auth_token.token}, status: :created
         else
             render json: @user.errors, status: :unprocessable_entity
         end
