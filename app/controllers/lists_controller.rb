ade5042e880cca56ed69b119432c182073290cd0
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-08-05 08:26:39 +1000

    add controller test

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,12 +2,16 @@ class ListsController < ApplicationController
     def create
         @list = List.create(list_params)
         if @list.save
-            render json: @list.title, status: :created
+            # would prefer to return just the title rather than the entire object
+            render json: @list, status: :created
         else
             render json: @list.errors, status: :unprocessable_entity
         end
     end
-
+    # edit
+    # destroy
+    # search
+    # search only Lists where public = true
 
     private
     def list_params
--- spec/requests/lists_spec.rb
+++ spec/requests/lists_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Lists", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "create new list" do
+    it '/create' do
+      # new_user = User.create(
+      #   first_name: "Test",
+      #   last_name: "User",
+      #   email: "test1@test.com",
+      #   username: "test123",
+      #   password: "Password1",
+      #   password_confirmation: "Password1",
+      #   date_of_birth: "2000-10-01"
+      # )
+
+      list_params = {
+        user_id: 1,
+        title: "Test List",
+        description: "This is a test list with a maximum length of 500 characters.
+        It can contain both Upper and Lower cases leters, numbers & special characters.",
+        public: false
+      }
+      post '/lists/create_list', :params => list_params.to_json, :headers => { "Content-Type": "application/json" }
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(201)
+    end
   end
 end
