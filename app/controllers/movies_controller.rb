200e068bccb8f0d54b011d780efd4af4ee235f99
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-08-10 14:10:47 +1000

    modify movies controller to render json for frontend

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,16 +3,24 @@ class MoviesController < ApplicationController
     end
 
     def search
-        @parameter = params[:query].downcase
+        # @parameter = params[:query].downcase
         #@results = Movie.result
         #puts @results
 
         # this should ideally be in the model but is in controller now for testing purposes
         require 'httparty'
+        # Retrieve API key stored in credentials
         api_key = Rails.application.credentials.tmdb[:api_key]
-        query = @parameter
-        result = HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&language=en-US&query=#{query}&page=1&include_adult=false")
+        # query = @parameter
+        # save query param as query variable and lowercase
+        query = params[:query].downcase
+        # API get request to TMDB using HTTParty
+        response = HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&language=en-US&query=#{query}&page=1&include_adult=false")
+        # destructure api response to return only results
+        @results = response["results"]
         # puts to console for testing purposes
-        puts result
+        # puts result
+        # render search results as json for frontent
+        render json: @results
     end
 end
